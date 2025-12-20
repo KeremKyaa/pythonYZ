941eec4aa3b4ea263ef14ad70c7a1e6f73bca5ba
krmky117 <krmky227@gmail.com>
2025-12-20 16:46:01 +0300

    SEPERATE, END, DOCSTRING, ESCAPE CHARACTER VARIABLE Formatting f Datatypes (str, number, boolean), STRING (DİZGİ) BOOLEAN (TRUE/FALSE) (Type == > list) (Type == > tuple) (Type == > set) (Type == > dict)
    GC_Cast
    Operators(aritmetik)

--- python_pure_tutorials/_01_print_comment.py
+++ python_pure_tutorials/_01_print_comment.py
@@ -1,38 +1,73 @@
-#bu proje yapay zeka için
+# Single Comment
+
 """
-multiple comment
+Multiple Comment
 """
 
+# =============================================
+# PRINT
+# =============================================
+print("Python Öğreniyorum")
 
-print("python öğreniyorum")
+print("Python", "Java", "Nodejs")
+print("Python" "Java" "Nodejs")
+# print("Python" "Java" "Nodejs")  # YAZAMAZSINIZ
 
-#float
-print(14.5)
+# Tam sayı
+print(1453)
 
+# Floating Point
+print(14.53)
+print(14, 53)
+
+# =============================================
 # SEPERATE
-print("merhabalar","güzel","insanlar",sep=" * ")
-#END
-print("Merhabalar","Python","Öğreniyorum"," *** ","Dünyasına Hoşgeldiniz")
+# =============================================
+print("Merhabalar", "Güzel", "İnsanlar", sep=" * ")
 
-print("Merhabalar","Python","Öğreniyorum",end=" *** ")
-print("Dünyasına Hoşgeldiniz")
+# =============================================
+# END
+# =============================================
+print("Merhabalar", "Python", "Öğreniyorum", "***", "Dünyasına hoşgeldiniz")
 
-#DOCSTRING
+print("Merhabalar", "Python", "Öğreniyorum", end=" *** ")
+print("Dünyasına hoşgeldiniz")
 
-print("""Merhabalar Python Öğreniyorum
-*** dünyasına hoşgeldiniz""")
+# =============================================
+# DOCSTRING
+# =============================================
+print("""
+Merhabalar Python Öğreniyorum *** Dünyasına hoşgeldiniz
+""")
 
-# ESCAPE /\
-print("""Merhabalar Python Öğreniyorum *** \n\tdünyasına hoşgeldiniz""")
+# =============================================
+# ESCAPE CHARACTER(\)    \n=new  \t=tab Formating
+# =============================================
+print("""
+Merhabalar Python Öğreniyorum *** \n\tDünyasına hoşgeldiniz
+""")
 
-#Değişken İsimlendirmelerde
+
+# =============================================
+# NON-ESCAPE CHARACTER(\) raw String(Ham String)
+# r: escape character(r devre dışı bırakmak için kullanıyoruz.)
+# =============================================
+print("C:\\User\\AppData")
+print(r"C:\\User\\AppData")
+
+# =============================================
+# VARIABLE
+# =============================================
+# Değişken isimlendirmelerde
 name = "Hamit"
 surname = "Mızrak"
+# print("Adım: ", name," \nSoyisim: ",surname)
 
-#print("Adım: " , name," \nSoyisim: ",surname)
 # 1.YOL
-print("Adım: " , name,"Soyisim: ",surname)
-# 2. yol (%s : String ,%d: decimal(Tam Sayılar) %f:float(Virgüllü Sayılar))
-print("Adım: %s Soyisim: %s:"%(name,surname))
+print("Adım: ", name," Soyisim: ",surname)
+
+# 2.YOL (%s:string  %d:decimal(tam sayılar)  %f:float(virgüllü sayılar))
+print("Adım:  %s  Soyisim:  %s "%(name,surname))
+
 # 3.YOL
-print(f"Adım: {name}, Soyisim: {surname}")
+print(f"Adım:  {name}, Soyisim:  {surname}")
