b04b5cbbac96e35e1d06163e2ac385bad75b7187
krmky117 <krmky227@gmail.com>
2025-12-20 14:36:46 +0300

    SEPERATE, END ,DOCSTRING,ESCAPE

---
+++ python_pure_tutorials/_01_print_comment.py
@@ -0,0 +1,26 @@
+#bu proje yapay zeka için
+"""
+multiple comment
+"""
+
+
+print("python öğreniyorum")
+
+#float
+print(14.5)
+
+# SEPERATE
+print("merhabalar","güzel","insanlar",sep=" * ")
+#END
+print("Merhabalar","Python","Öğreniyorum"," *** ","Dünyasına Hoşgeldiniz")
+
+print("Merhabalar","Python","Öğreniyorum",end=" *** ")
+print("Dünyasına Hoşgeldiniz")
+
+#DOCSTRING
+
+print("""Merhabalar Python Öğreniyorum
+*** dünyasına hoşgeldiniz""")
+
+# ESCAPE /\
+print("""Merhabalar Python Öğreniyorum *** \n\tdünyasına hoşgeldiniz""")
