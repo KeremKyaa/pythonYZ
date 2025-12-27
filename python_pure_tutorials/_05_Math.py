545c95b2646b6bdcfd03237d30d868438a1e9405
krmky117 <krmky227@gmail.com>
2025-12-27 14:20:06 +0300

    Math

--- python_pure_tutorials/_05_Math.py
+++ python_pure_tutorials/_05_Math.py
@@ -8,9 +8,21 @@ import math
 print("PI: ", math.pi)
 print("E: ", math.e)
 
+#######################################################
+####### MATH ##########################################
+import math
+from random import randint, uniform
+
 
 #######################################################
 ####### SABİT #########################################
+print("PI: ", math.pi)
+print("E: ", math.e)
+
+
+
+#######################################################
+####### KAREKÖK/ÜSLÜ/FAKTÖRİYEL #######################
 print("Karekök: ", math.sqrt(16))
 print("üslü: ", math.pow(2,5))
 print("üslü: ", (2**5))
@@ -22,13 +34,24 @@ print("Aşağı yuvarla: ", math.floor(4.9))
 
 #######################################################
 ####### LOGARITMIK ####################################
-print("Log: ", math.log(100))
+print("Logaritma: " , str(math.log(100)))
+print("Sinüs: " , str(math.sin(math.radians(90))))
+print("Kosinüs: " , str(math.cos(math.radians(90))))
+print("Trigonometrik açıların radyan cevresi: " , str(math.radians(90)))
+print("Trigonometrik açıların derece cevresi: " , str(math.degrees(math.pi/2)))
+
 
 
 #######################################################
-####### LOGARITMIK ####################################
+####### AGGREAGE ######################################
 number_list=[1,2,3,4,5]
 print("En büyük: ", max(number_list))
 print("En küçük: ", min(number_list))
 print("Sum: ", sum(number_list))
 print("Average: ", sum(number_list)/len(number_list))
+
+
+#######################################################################################
+#### Sayısal İşlemler Rastgele  #######################################################
+print("Rastgele Tam sayı: ",randint(1,100))
+print("Rastgele Ondalık sayı: ",uniform(0,1))
